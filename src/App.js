482f70d667e34b8f9cd07408785262458069c4ed
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-11-18 12:49:52 +0545

    -used hooks to connect to redux-store

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import CakeContainer from "./components/CakeContainer";
 import React from "react";
 import {Provider} from "react-redux"
 import store from "./redux/store";
+import HooksCakeContainer from "./components/HooksCakeContainer";
 function App() {
     return (
         <Provider store={store}>
             <div className="App">
+                <HooksCakeContainer/>
                 <CakeContainer/>
             </div>
         </Provider>
