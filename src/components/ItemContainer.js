34c1d9a3eeca3838172c23b44d905528d4b7d9fc
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-11-19 17:40:55 +0545

    -action multiple parametr in map dispatch to props

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {connect} from "react-redux";
+import {buyCake,buyIceCream} from "../redux";
 
 function ItemContainer(props) {
     return (
         <div>
             <h2>Item - {props.item}</h2>
+            <button onClick={props.buyItem}>Buy Items</button>
         </div>
     );
 }
@@ -15,6 +17,11 @@ const mapStateToProps = (state, ownProps) => {
         item: itemState
     }
 }
+const mapDispatchToProps = (dispatch,ownProps)=>{
+    const dispatchFunction=ownProps.cake? ()=>dispatch(buyCake()):()=>dispatch(buyIceCream())
+return{
+        buyItem:dispatchFunction
+}
+}
 
-
-export default connect(mapStateToProps) (ItemContainer);
+export default connect(mapStateToProps,mapDispatchToProps) (ItemContainer);
