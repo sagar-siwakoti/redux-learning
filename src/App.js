d4fbd411dd00bb955f0e9136abf1bbf1cd4e80b6
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-11-19 17:33:36 +0545

    -action multiple parameters in mapstate to props

--- src/App.js
+++ src/App.js
@@ -7,15 +7,19 @@ import HooksCakeContainer from "./components/HooksCakeContainer";
 import IceCreamContainer from "./components/IceCreamContainer";
 import ExampleContainer from "./components/exampleFeature";
 import NewCakeContainer from "./components/NewCakeContainer";
+import ItemContainer from "./components/ItemContainer";
 function App() {
     return (
         <Provider store={store}>
             <div className="App">
+                <ItemContainer cake/>
+                <ItemContainer />
                 <HooksCakeContainer/>
                 <CakeContainer/>
                 <IceCreamContainer/>
                 <ExampleContainer/>
                 <NewCakeContainer/>
+
             </div>
         </Provider>
     );
---
+++ src/components/ItemContainer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {connect} from "react-redux";
+
+function ItemContainer(props) {
+    return (
+        <div>
+            <h2>Item - {props.item}</h2>
+        </div>
+    );
+}
+
+const mapStateToProps = (state, ownProps) => {
+    const itemState = ownProps.cake ? state.cake.numOfCakes : state.iceCream.numOfIceCream
+    return {
+        item: itemState
+    }
+}
+
+
+export default connect(mapStateToProps) (ItemContainer);
